8de515975c12b34fb8ebb325e7618b9f39f599a2
ikikika <iki.jito@gmail.com>
2018-04-06 23:54:37 +0800

    alternate counter colours

--- app/src/main/java/com/jitooi/tictactoe/MainActivity.java
+++ app/src/main/java/com/jitooi/tictactoe/MainActivity.java
@@ -7,13 +7,25 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //0: yellow
+    //1: red
+    int activePlayer = 0;
+
     public void dropIn(View view){
 
         ImageView counter = (ImageView) view;
 
         counter.setTranslationY(-1500);
 
-        counter.setImageResource(R.drawable.yellow);
+        if( activePlayer == 0 ){
+            counter.setImageResource(R.drawable.yellow);
+            activePlayer = 1;
+        } else {
+            counter.setImageResource(R.drawable.red);
+            activePlayer = 0;
+        }
+
+
 
         counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
 
