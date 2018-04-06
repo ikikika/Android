6797c73a2776bf7ec00e4cf9b479f958a6cd4809
ikikika <iki.jito@gmail.com>
2018-04-07 04:26:39 +0800

    check who is winner

--- app/src/main/java/com/jitooi/tictactoe/MainActivity.java
+++ app/src/main/java/com/jitooi/tictactoe/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     //2: empty
     int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
 
-
+    int[][] winningPositions = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
 
     public void dropIn(View view){
 
@@ -36,7 +39,21 @@ public class MainActivity extends AppCompatActivity {
 
         counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
 
+        //Log.i("gameState", Arrays.toString(gameState));
 
+        for( int[] winningPosition: winningPositions ){
+
+            if( gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2 ){
+                //someone has won
+                String winner = "";
+                if( activePlayer == 1 ){
+                    winner = "yellow";
+                } else {
+                    winner = "red";
+                }
+                Toast.makeText(this, winner+" has won", Toast.LENGTH_SHORT).show();
+            }
+        }
 
     }
 
