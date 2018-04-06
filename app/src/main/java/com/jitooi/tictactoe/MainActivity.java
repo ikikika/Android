a24cb81bae8b4312ca51c6789d430f315678e39a
ikikika <iki.jito@gmail.com>
2018-04-07 04:08:37 +0800

    define gamestate

--- app/src/main/java/com/jitooi/tictactoe/MainActivity.java
+++ app/src/main/java/com/jitooi/tictactoe/MainActivity.java
@@ -2,6 +2,7 @@ package com.jitooi.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -11,9 +12,17 @@ public class MainActivity extends AppCompatActivity {
     //1: red
     int activePlayer = 0;
 
+    //2: empty
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+
+
+
     public void dropIn(View view){
 
         ImageView counter = (ImageView) view;
+
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+        gameState[tappedCounter] = activePlayer;
 
         counter.setTranslationY(-1500);
 
@@ -25,9 +34,9 @@ public class MainActivity extends AppCompatActivity {
             activePlayer = 0;
         }
 
-
-
         counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+
+
 
     }
 
