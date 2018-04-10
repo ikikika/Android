72aab5c979e2306d031979c406e55c856968b8cf
ikikika <iki.jito@gmail.com>
2018-04-11 00:47:23 +0800

    set up url connection

--- app/src/main/java/com/jitooi/android_json_demo/MainActivity.java
+++ app/src/main/java/com/jitooi/android_json_demo/MainActivity.java
@@ -4,13 +4,40 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     public class DownloadTask extends AsyncTask<String, Void, String>{
 
         @Override
-        protected String doInBackground(String... strings) {
-            return null;
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while ( data != -1 ){
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+                return result;
+
+            } catch (Exception e){
+                e.printStackTrace();
+                return null;
+            }
         }
     }
 
