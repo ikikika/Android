3aca6ddc1b884730a7f6b36239e6f67253f0c7d7
ikikika <iki.jito@gmail.com>
2018-04-13 22:06:45 +0800

    refresh app, set up listview with list of names

--- app/src/main/java/com/jitooi/multipleactivitydemo/SecondActivity.java
+++ app/src/main/java/com/jitooi/multipleactivitydemo/SecondActivity.java
@@ -7,16 +7,15 @@ import android.view.View;
 import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
-
+/*
     public void goBack( View view){
         //this code will generate a new screen everytime, instead of bring us back
 //        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
 //        startActivity(intent);
         //use this code to bring us back
         finish();
-
     }
-
+*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
