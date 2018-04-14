6c6ebc695b55eefbf9929b9bd105564b65874705
ikikika <iki.jito@gmail.com>
2018-04-14 23:59:11 +0800

    get user location, add marker, zoom in on location

--- app/src/main/java/com/jitooi/memorableplaces/MapsActivity.java
+++ app/src/main/java/com/jitooi/memorableplaces/MapsActivity.java
@@ -1,8 +1,17 @@
 package com.jitooi.memorableplaces;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -14,7 +23,33 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    LocationManager locationManager;
+    LocationListener locationListener;
     private GoogleMap mMap;
+
+    public void centerMapOnLocation(Location location, String title){
+        //check if initial location is set
+        if( location != null ){
+            LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+            mMap.clear();
+            mMap.addMarker( new MarkerOptions().position(userLocation).title(title) );
+            mMap.moveCamera( CameraUpdateFactory.newLatLngZoom(userLocation, 13) );
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if( grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED ){ // check if general permissions granted
+            if( ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ){ //confirm if location permission granted (the right one)
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+                //when we have permission for the very first time, get last know location
+                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                centerMapOnLocation(lastKnownLocation, "Your location");
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,11 +76,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         Intent intent = getIntent();
-        Toast.makeText(this, Integer.toString(intent.getIntExtra("placeNumber", 0)), Toast.LENGTH_SHORT).show();
+        if( intent.getIntExtra("placeNumber", 0) == 0 ){
+            //zoom in on user location
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    centerMapOnLocation(location, "Your location");
+                }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+
+                }
+            };
+
+            //check if have permission
+            if( ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ){ //confirm if location permission granted (the right one)
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+                // if have permission, get last known location and get everything set up
+                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                centerMapOnLocation(lastKnownLocation, "Your location");
+            } else { //if no permission granted
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+
+        }
+
     }
 }
