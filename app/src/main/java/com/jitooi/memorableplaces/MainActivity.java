7a86f5339dcf8fbe7e64b4c042b55c72af648b33
ikikika <iki.jito@gmail.com>
2018-04-14 23:21:02 +0800

    link listview in xml to java, array list for items, array adapter to connect listview to array, do something when item tapped in listview

--- app/src/main/java/com/jitooi/memorableplaces/MainActivity.java
+++ app/src/main/java/com/jitooi/memorableplaces/MainActivity.java
@@ -2,6 +2,13 @@ package com.jitooi.memorableplaces;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+
+        ArrayList<String> places = new ArrayList<String>();
+
+        places.add("Add a new place...");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, places);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, Integer.toString(i), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
