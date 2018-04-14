9e77c74375154040c3f0c8e245f955b80c3ede8f
ikikika <iki.jito@gmail.com>
2018-04-14 23:39:20 +0800

    open maps when tap on item in listview

--- app/src/main/java/com/jitooi/memorableplaces/MapsActivity.java
+++ app/src/main/java/com/jitooi/memorableplaces/MapsActivity.java
@@ -1,7 +1,9 @@
 package com.jitooi.memorableplaces;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -37,6 +39,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        Intent intent = getIntent();
+        Toast.makeText(this, Integer.toString(intent.getIntExtra("placeNumber", 0)), Toast.LENGTH_SHORT).show();
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
