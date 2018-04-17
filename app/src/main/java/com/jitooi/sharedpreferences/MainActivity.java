b7f121f3a29723cc11b1e397717cab9d24ba0588
ikikika <iki.jito@gmail.com>
2018-04-17 15:12:49 +0800

    store string in shared preferences

--- app/src/main/java/com/jitooi/sharedpreferences/MainActivity.java
+++ app/src/main/java/com/jitooi/sharedpreferences/MainActivity.java
@@ -1,7 +1,10 @@
 package com.jitooi.sharedpreferences;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.jitooi.sharedpreferences", Context.MODE_PRIVATE);
+
+        sharedPreferences.edit().putString("username", "ikikika").apply(); //first string is name of string, second string is value
+        String username = sharedPreferences.getString("username", "no name"); // first is name, second string is default
+
+        Log.i("This is username", username);
     }
 }
