bba2fb983f45fd91818343d287b6f7cd33b20274
ikikika <iki.jito@gmail.com>
2018-04-17 15:35:27 +0800

    make objectserializer, serialize arraylist and deserialize

--- app/src/main/java/com/jitooi/sharedpreferences/MainActivity.java
+++ app/src/main/java/com/jitooi/sharedpreferences/MainActivity.java
@@ -6,6 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static com.jitooi.sharedpreferences.ObjectSerializer.serialize;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,9 +20,32 @@ public class MainActivity extends AppCompatActivity {
 
         SharedPreferences sharedPreferences = this.getSharedPreferences("com.jitooi.sharedpreferences", Context.MODE_PRIVATE);
 
+        ArrayList<String> friends = new ArrayList<>();
+        friends.add("ashley");
+        friends.add("bucky");
+        friends.add("Claudia");
+
+        try {
+            sharedPreferences.edit().putString( "friendsList", ObjectSerializer.serialize(friends) ).apply();
+
+            Log.i("serizlized friends list", ObjectSerializer.serialize(friends) );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<String> newFriends = new ArrayList<>();
+        try {
+            newFriends = (ArrayList<String>) ObjectSerializer.deserialize( sharedPreferences.getString("friendsList", ObjectSerializer.serialize(new ArrayList<String>())) );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Log.i("new friends", newFriends.toString() );
+        /*
+        //run once, after that if comment out this line, value will still be saves in shared preferences
         sharedPreferences.edit().putString("username", "ikikika").apply(); //first string is name of string, second string is value
         String username = sharedPreferences.getString("username", "no name"); // first is name, second string is default
 
         Log.i("This is username", username);
+        */
     }
 }
