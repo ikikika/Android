b68dcb0b5def008d768d1765b8d9240b47a3da50
ikikika <iki.jito@gmail.com>
2018-04-20 00:03:13 +0800

    initial commit

---
+++ app/src/main/java/com/jitooi/languagepreferences/MainActivity.java
@@ -0,0 +1,17 @@
+package com.jitooi.languagepreferences;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
