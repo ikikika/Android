e5e5c1eaeb87d17f830567323465e72652c8be20
ikikika <iki.jito@gmail.com>
2018-04-19 23:39:55 +0800

    create menu resource file, add items to main_menu.xml, set up menu inflater

--- app/src/main/java/com/jitooi/menudemo/MainActivity.java
+++ app/src/main/java/com/jitooi/menudemo/MainActivity.java
@@ -2,8 +2,17 @@ package com.jitooi.menudemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
