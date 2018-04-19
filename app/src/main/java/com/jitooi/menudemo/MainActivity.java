395ee03b2af9cad9986feea34c620f8306fdc5a4
ikikika <iki.jito@gmail.com>
2018-04-19 23:44:30 +0800

    call action when item selected

--- app/src/main/java/com/jitooi/menudemo/MainActivity.java
+++ app/src/main/java/com/jitooi/menudemo/MainActivity.java
@@ -2,8 +2,10 @@ package com.jitooi.menudemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,24 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        super.onOptionsItemSelected(item);
+
+        //check if item selected exist and get the item id
+        switch( item.getItemId() ){
+            case R.id.settings:
+                Log.i("Item Selected", "Settings");
+                return true;
+            case R.id.help:
+                Log.i("Item Selected", "Settings");
+                return true;
+            default:
+                return false;
+        }
     }
 
     @Override
