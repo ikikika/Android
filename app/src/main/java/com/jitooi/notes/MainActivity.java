872bf6e425901ac303a34c4bd3902410cdb7e394
ikikika <iki.jito@gmail.com>
2018-05-15 03:00:08 +0800

    delete note

--- app/src/main/java/com/jitooi/notes/MainActivity.java
+++ app/src/main/java/com/jitooi/notes/MainActivity.java
@@ -1,5 +1,7 @@
 package com.jitooi.notes;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -37,6 +39,31 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                final int itemToDelete = i;
+
+                new AlertDialog.Builder(MainActivity.this)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle("Are you sure?")
+                        .setMessage("Do you want to delete this note?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                notes.remove(itemToDelete);
+                                arrayAdapter.notifyDataSetChanged();
+                            }
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
+
+                return true; //whether or not to allow long press to happen
+            }
+        });
+
     }
 
     //menu code
