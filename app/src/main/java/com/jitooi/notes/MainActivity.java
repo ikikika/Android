0021534b2ce99e27b00b772173e47e47bcd901e2
ikikika <iki.jito@gmail.com>
2018-05-15 01:51:43 +0800

    menu, add note button, direct to NoteEditorActivity

--- app/src/main/java/com/jitooi/notes/MainActivity.java
+++ app/src/main/java/com/jitooi/notes/MainActivity.java
@@ -3,6 +3,9 @@ package com.jitooi.notes;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -34,5 +37,24 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    //menu code
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if( item.getItemId() == R.id.add_note ){
+            Intent intent = new Intent(getApplicationContext(), NoteEditorActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return false;
     }
 }
