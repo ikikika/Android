1d6e8200b1092a4340daea8e4d95a0546c637207
ikikika <iki.jito@gmail.com>
2018-05-24 16:10:13 +0800

    loginphpmysql create layout for registration

---
+++ loginphpmysql/front/app/src/main/java/com/jitooi/loginphpmysql/Constants.java
@@ -0,0 +1,8 @@
+package com.jitooi.loginphpmysql;
+
+public class Constants {
+
+    private static final String ROOT_URL = "http://192.168.1.196/Android/Android/loginphpmysql/back/v1/";
+    public static final String URL_REGISTER = ROOT_URL + "registerUser.php";
+
+}
