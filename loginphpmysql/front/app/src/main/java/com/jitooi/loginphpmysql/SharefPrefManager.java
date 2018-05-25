bf13664c8c8a8b6c1ffae9c2ccd697143a692f6a
ikikika <iki.jito@gmail.com>
2018-05-25 12:59:52 +0800

    loginphpmysql class to store user data & logged in or not in shared pref

---
+++ loginphpmysql/front/app/src/main/java/com/jitooi/loginphpmysql/SharefPrefManager.java
@@ -0,0 +1,60 @@
+package com.jitooi.loginphpmysql;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class SharefPrefManager {
+    private static SharefPrefManager mInstance;
+    private static Context mCtx;
+
+    private static final String SHARED_PREF_NAME = "mysharefpref12";
+    private static final String KEY_USERNAME = "username";
+    private static final String KEY_USER_EMAIL = "useremail";
+    private static final String KEY_USER_ID = "userid";
+
+    private SharefPrefManager(Context context) {
+        mCtx = context;
+    }
+
+    public static synchronized SharefPrefManager getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new SharefPrefManager(context);
+        }
+        return mInstance;
+    }
+
+    public boolean userLogin(int id, String username, String email){
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putInt(KEY_USER_ID, id);
+        editor.putString(KEY_USER_EMAIL, email);
+        editor.putString(KEY_USERNAME, username);
+
+        editor.apply();
+
+        return true;
+    }
+
+    public boolean isLoggedIn(){
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        if( sharedPreferences.getString(KEY_USERNAME, null) != null ){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean logout(){
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.clear();
+        editor.apply();
+        
+        return true;
+    }
+}
