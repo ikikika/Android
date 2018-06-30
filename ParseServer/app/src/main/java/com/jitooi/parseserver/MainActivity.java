78e1a1b10075c1a0bfed4d47aace50c4cb6fe270
ikikika <iki.jito@gmail.com>
2018-07-01 02:18:11 +0800

    parseserver connect to parse server

---
+++ ParseServer/app/src/main/java/com/jitooi/parseserver/MainActivity.java
@@ -0,0 +1,20 @@
+package com.jitooi.parseserver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.ParseInstallation;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ParseAnalytics.trackAppOpenedInBackground(getIntent());
+    }
+}
