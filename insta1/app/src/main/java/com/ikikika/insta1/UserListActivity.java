7a3be0925fe3008f39ff8757907af123ffdd6fbb
ikikika <iki.jito@gmail.com>
2019-05-20 21:29:54 +0800

    form arrayList and arrayAdapter to test populate listView

--- insta1/app/src/main/java/com/ikikika/insta1/UserListActivity.java
+++ insta1/app/src/main/java/com/ikikika/insta1/UserListActivity.java
@@ -2,6 +2,10 @@ package com.ikikika.insta1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class UserListActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class UserListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list);
+
+        ListView listView = findViewById(R.id.listView);
+        ArrayList<String> usernames = new ArrayList<String>();
+        usernames.add("test");
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, usernames);
+        listView.setAdapter(arrayAdapter);
     }
 }
